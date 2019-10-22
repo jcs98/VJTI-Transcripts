324c904c6234f502ac4dc813c730bf0e7b3c29a6
Jainam C Shah <jainamcshah98@gmail.com>
2019-10-23 00:25:04 +0530

    Initial commit

---
+++ Transcripts.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.5.0;
+
+contract Transcripts {
+    address payable public owner;
+    // mapping from student id to cpi
+    mapping(bytes32 => bytes32) private transcripts;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    // helper hash function
+    function hash(string memory _str) private pure returns(bytes32) {
+        return  keccak256(abi.encode(_str));
+    }
+    
+    function addTranscript(string memory _studentId, string memory _CPI) public {
+        // only owner can add transcripts
+        require(msg.sender == owner);
+        transcripts[hash(_studentId)] = hash(_CPI);
+    }
+    
+    function verifyTranscript(string memory _studentId, string memory _CPI) public view returns(bool) {
+        return (transcripts[hash(_studentId)] == hash(_CPI));
+    }
+}
